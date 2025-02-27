a5135de969b3fbbab0cd4e5ceb35cd3c06092152
RohitKumarVadlamudi <98195539+RohitKumarVadlamudi@users.noreply.github.com>
2025-02-27 00:10:23 -0500

    Added card update option
    Added card update option in card details page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect,request, url_for
 
 app= Flask(__name__)
 
@@ -21,12 +21,30 @@ def home():
 @app.route('/card/<int:card_id>')
 def card_details(card_id):
     card = cards[card_id]
-    return render_template('card_details.html', card=card)
+    return render_template('card_details.html', card=card, card_id=card_id)
 
 
 @app.route('/cards')
 def card_list():
 	return render_template('cards.html', cards=cards)
+
+@app.route('/update_card/<int:card_id>', methods=['POST'])
+def update_card(card_id):
+    card = cards[card_id]
+    
+    # Update due date
+    card['due_date'] = request.form['due_date']
+    try:
+    	card['min_payment'] = int(request.form['min_payment'])
+    except:
+    	card['min_payment']=card['min_payment']
+    
+    # Update payment status
+    status = request.form['status']
+    card['paid'] = True if status == 'paid' else False
+    
+    return redirect(url_for('card_details', card_id=card_id))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
