0d6e9fcf5d3e1d644d47a5a30e1f9126ef00fa2e
RohitKumarVadlamudi <98195539+RohitKumarVadlamudi@users.noreply.github.com>
2025-03-03 00:06:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/your_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4EVERre:monster@localhost/card_manager'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
